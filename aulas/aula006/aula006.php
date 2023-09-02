d60aeb24817cc02ee00492ed09342c8ca76b95e4
Felipe Pinheiro <flppinheiro@hotmail.com>
2023-09-02 20:51:06 -0300

    Aula 6 - Objetos compostos em PHP

---
+++ aulas/aula006/aula006.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula 006 - POO</title>
+    <style>
+        * {
+            margin: 0px;
+            padding: 0px;
+            
+        }
+
+        header {
+            background-color: black;
+        }
+        
+        header > h1, h2{
+            color: red;
+            font-family: 'Courier New', Courier, monospace;
+        }
+        
+        main {
+            font-size: 16px;
+            padding: 10px;
+        }
+    </style>
+</head>
+<body>
+    <header>
+        <h1>ULTIMATE EMOJI COMBAT</h1>
+        <h1>UEC</h1>
+    </header>
+    <main>
+        <pre>
+        <?php 
+            require_once 'lutador.php';
+            // Declarando um Array para receber varios lutadores
+            $Lutador = array();
+            
+            $Lutador[0] = new Lutador("Putscript", "Brasil", 29, 1.68, 57.8, 14, 2, 3);
+            
+            $Lutador[1] = new Lutador("Pretty Boy", "França", 31, 1.75, 68.9, 11, 2, 1);
+
+            $Lutador[2] = new Lutador("SnapShadow", "EUA", 35, 1.65, 80.9, 12, 2, 1);
+
+            $Lutador[3] = new Lutador("DeadCode", "Austrália", 28, 1.93, 81.6, 13, 0, 2);
+
+            $Lutador[4] = new Lutador("UFCobol", "Brasil", 37, 1.70, 119.3, 5, 4, 3);
+
+            $Lutador[5] = new Lutador("Nerdaart", "EUA", 30, 1.81, 105.7, 12, 2, 4);
+            
+            $Lutador[5]->apresentar();
+  
+        ?>
+        </pre>
+    </main>
+</body>
+</html>
---
+++ aulas/aula006/lutador.php
@@ -0,0 +1,173 @@
+<?php 
+//   LUTADOR
+// ------------------
+// - nome
+// - nacionalidade
+// - idade 
+// - altura
+// - peso
+// - categoria
+// - vitorias
+// - derrotas
+// - empates
+// -----------------
+// + apresentar()
+// + status()
+// + ganharLuta()
+// + perderLuta()
+// + empatarLuta()
+
+class Lutador {
+    // ATRIBUTOS
+    private $nome;
+    private $nacionalidade;
+    private $idade;
+    private $altura;
+    private $peso;
+    private $categoria;
+    private $vitorias;
+    private $derrotas;
+    private $empates;
+
+    // FUNÇÕES TOP DA BALADA
+    public function apresentar() {
+        echo "<p>O Lutador <strong>" . $this->getNome() . "</strong> ";
+        echo "vem lá do<sup>(a)</sup> <strong>" . $this->getNacionalidade() . "</strong> ";
+        echo "ele tem <strong>" . $this->getIdade() . " anos</strong>.</p>";
+        echo "<p>Medindo exatamente <strong>" . number_format($this->getAltura(), 2) . "<sup>m</sup></strong> de altura.</p>";
+        echo "<p>Pesando exatamente <strong>" . $this->getPeso() . "<sup>KG</sup>.</strong></p>";
+        echo "<p>Da categoriaaaa <strong>" . $this->getCategoria() . "</strong> Senhoras e Senhores.</p>";
+        echo "<p>Com exatamente <strong>" . $this->getVitorias() . " Vitórias, ";
+        echo $this->getDerrotas() . " Derrotas, ";
+        echo "Eeeee " . $this->getEmpates() . " Empates.</strong></p>";
+    }
+
+    public function status() {
+        echo "<p>Lutador: " . $this->getNome(). "</p>";    
+        echo "<p>Peso: " . $this->getCategoria() . "</p>";
+        echo "<p>Vitórias: " . $this->getVitorias() . "</p>";
+        echo "<p>Derrotas: " . $this->getDerrotas() . "</p>";
+        echo "<p>Empates: " . $this->getEmpates() . "</p>";   
+    }
+
+    public function ganharLuta() {
+        return $this->setVitorias($this->getVitorias() + 1);
+    }
+
+    public function perderLuta() {
+        return $this->setDerrotas($this->getDerrotas() + 1);
+    }
+
+    public function empatarLuta() {
+        return $this->setEmpates($this->getEmpates() + 1);
+    }
+
+    // CONSTRUTOR
+    public function __construct(
+    $nome, 
+    $naci, 
+    $idade, 
+    $altura, 
+    $peso, 
+    $vitorias, 
+    $derrotas, 
+    $empates)
+    {
+     $this->setNome($nome);
+     $this->setNacionalidade($naci);
+     $this->setIdade($idade);
+     $this->setAltura($altura);
+     $this->setPeso($peso);
+     // Categoria automático
+     $this->setVitorias($vitorias);
+     $this->setDerrotas($derrotas);
+     $this->setEmpates($empates);   
+    }
+
+    // SETTERS
+    public function setNome($nome) {
+        $this->nome = $nome;
+    }
+    
+    public function setNacionalidade($naci) {
+        $this->nacionalidade = $naci;
+    }
+    
+    public function setIdade($idade) {
+        $this->idade = $idade;
+    }
+    
+    public function setAltura($altura) {
+        $this->altura = $altura;
+    }
+    
+    public function setPeso($peso) {
+        $this->peso= $peso;
+        $this->setCategoria();
+    }
+    
+    public function setCategoria() {
+        if ($this->getPeso() < 52.2) {
+            $this->categoria = "Inválido";
+        }elseif ($this->getPeso() <= 73.3) {
+            $this->categoria = "Leve";
+        }elseif ($this->getPeso() <= 83.9) {
+            $this->categoria = "Médio";
+        }elseif ($this->getPeso() <= 120.2) {
+            $this->categoria = "Pesado";
+        }else {
+            $this->categoria = "Inválido";
+        }
+    }
+    
+    public function setVitorias($vitorias) {
+        $this->vitorias = $vitorias;
+    }
+    
+    public function setDerrotas($derrotas) {
+        $this->derrotas = $derrotas;
+    }
+    
+    public function setEmpates($empates) {
+        $this->empates = $empates;
+    }
+
+    // GETTERS
+    public function getNome() {
+        return $this->nome;
+    }
+    
+    public function getNacionalidade() {
+        return $this->nacionalidade;
+    }
+    
+    public function getIdade() {
+        return $this->idade;
+    }
+    
+    public function getAltura() {
+        return $this->altura;
+    }
+    
+    public function getPeso() {
+        return $this->peso;
+    }
+    
+    public function getCategoria() {
+        return $this->categoria;
+    }
+    
+    public function getVitorias() {
+        return $this->vitorias;
+    }
+    
+    public function getDerrotas() {
+        return $this->derrotas;
+    }
+    
+    public function getEmpates() {
+        return $this->empates;
+    }
+}
+
+?>
